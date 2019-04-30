642bac2a9591c6ee924b709541a0b130b7d89088
Luca Pasquale <lucapasquale.094@gmail.com>
2019-04-30 14:21:23 -0300

    jq on basic repositories

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Basics
-sudo apt install git curl fonts-firacode -y
+sudo apt install -y git curl fonts-firacode jq
 
 ############################################
 #                 VSCode
@@ -14,7 +14,7 @@ sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
-sudo apt install code -y
+sudo apt install -y code
 
 ###########################################
 #               Node.js
@@ -28,7 +28,7 @@ sudo apt-get install -y nodejs
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn -y
+sudo apt-get update && sudo apt-get install -y yarn
 
 # Global packages
 sudo npm install -g n
@@ -59,7 +59,7 @@ echo "Installing tools..."
 # Insomnia
 echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
-sudo apt-get update && sudo apt-get install insomnia -y
+sudo apt-get update && sudo apt-get install -y insomnia
 
 # Ripgrep
 sudo apt-get install ripgrep
