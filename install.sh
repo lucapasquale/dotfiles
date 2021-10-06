d153ccdf307fdf0bcc7945a1bea56723d7a8fa8f
Luca Pasquale <lucapasquale.094@gmail.com>
2021-10-05 22:16:26 -0300

    installing zsh before asdf, setting zsh as default shell for tmux

--- install.sh
+++ install.sh
@@ -4,6 +4,25 @@
 sudo apt-get install -y git curl jq fonts-firacode
 rm -rf ~/.gitconfig && ln -s $PWD/.gitconfig ~/.gitconfig
 
+############################################
+#          Oh My Zsh and plugins
+############################################
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --no-update-rc
+
+# zsh
+sudo apt-get install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+rm -rf ~/.zshrc && ln -s $PWD/.zshrc ~/.zshrc
+
+# plugins
+sudo apt-get install -y git-extras
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 
 ###########################################
 #               Node.js
@@ -23,26 +42,6 @@ sudo apt-get update && sudo apt-get install -y yarn
 
 # global packages
 npm install -g typescript nodemon eslint prettier ngrok
-
-
-############################################
-#          Oh My Zsh and plugins
-############################################
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-update-rc
-
-# zsh
-sudo apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-rm -rf ~/.zshrc && ln -s $PWD/.zshrc ~/.zshrc
-
-# plugins
-sudo apt-get install -y git-extras
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
 ############################################
